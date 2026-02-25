d41290bba523f5284618e733e0616e3d45e91c71
Amir Djelidi <amir.djelidi@efrei.net>
2026-02-25 10:13:02 +0100

    Remove background service worker and related permissions

--- background.js
+++
@@ -1,4 +0,0 @@
-// background.js
-// Plus de vérification automatique.
-// Le fichier reste là au cas où on voudrait ajouter des fonctionnalités de fond plus tard.
-console.log("Scan Tracker : Service Worker inactif (Mode Manuel).");
